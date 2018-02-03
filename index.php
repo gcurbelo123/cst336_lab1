d4bcd24f70560204cade01ac2f144c2c3eb40028
Gilbert Curbelo <gcurbelo@csumb.edu>
2018-02-03 00:20:15 +0000

    Updated CSS and HTML Pages

--- index.php
+++ index.php
@@ -24,20 +24,20 @@ Personal Portfolio
     <!-- This is where we place the content of our website -->
     <body>
         
-        <header>
+        <header id = "name">
             <h1> Gilbert Curbelo </h1>
         </header>
         <nav>
-            <hr width = "35%"/>
+            <hr width = "0%"/>
             
-            <a href = "index.php">Home</a>
+            <span id = "currentLink"><a href = "index.php">Home</a></span>
             <a href = "about.html">About</a>
             <a href = "contact.html">Contact</a>
         </nav>
         
         <main>
                 <figure id = "me">
-                    <img src = "img/gilbert_curbelo.png" alt = "Picture of Gilbert Curbelo" />
+                    <img src = "img/gilbert_curbelo.png" style="max-width:100%;height:auto;"/>
                 </figure>
                 <div id = "welcomeText">
                     <p>
@@ -60,10 +60,10 @@ Personal Portfolio
         <!-- The footer goes inside the body but not always -->
         <footer>
                 <hr>
-                    2017 - Curbelo
+                    &copy 2017 - Curbelo
                 <br/>
                 <figure id = "logo">
-                    <img src = "img/csumb_logo.png" alt = "CSUMB Logo" />
+                    <img src = "img/csumb_logo2.png" alt = "CSUMB Logo" />
                 </figure>
         </footer>
         <!-- closing footer -->
